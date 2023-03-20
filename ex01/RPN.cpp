4040f21540b96ad69c30fcf6f29de810baf0e296
Elise Colin <ecolin@student.42.fr>
2023-03-20 10:33:22 +0100

    RPN done

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -3,43 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecolin <ecolin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elise <elise@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:24:50 by elise             #+#    #+#             */
-/*   Updated: 2023/03/17 17:17:32 by ecolin           ###   ########.fr       */
+/*   Updated: 2023/03/20 10:29:10 by elise            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
-
-Arg::Arg(int &num, char op): num(num), op(op)
-{
-	
-}
-
-Arg::Arg(): num(11), op('\0')
-{
-	
-}
-
-Arg::~Arg()
-{
-	
-}
-
-Arg::Arg(int num): num(num)
-{
-	op = '\0';
-}
-
-Arg::Arg(Arg const &a): num(a.num), op(a.op)
-{
-	
-}
-
-Arg &Arg::operator=(Arg const &a)
-{
-	this->num = a.num;
-	this->op = a.op;
-	return (*this);
-}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecolin <ecolin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elise <elise@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:25:10 by elise             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/03/18 18:34:37 by elise            ###   ########.fr       */
-=======
-/*   Updated: 2023/03/17 17:28:48 by ecolin           ###   ########.fr       */
->>>>>>> e29fa65f1e2eff51dc44fc566632c1eb6af85e86
+/*   Updated: 2023/03/20 10:29:12 by elise            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +46,6 @@ int main(int argc, char *argv[])
         std::cout << "Bad Input.\n";
         exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
     int i = 1;
     std::list<int> tab;
     tab.push_back(argv[1][0]);
@@ -86,37 +81,4 @@ int main(int argc, char *argv[])
         it++;
     }
     std::cout << *tab.begin() - '0';
-=======
-    std::string s = argv[1];
-    size_t  pos = s.find_first_not_of(' ');
-    std::vector<int> tab;
-	std::ve
-    pos = s.find_first_not_of(' ');
-    while (pos != s.npos)
-    {
-        if (isdigit(s[pos]))
-        {
-            if (std::atoi(&s[pos]) > 10)
-            {
-                std::cout << "Bad Input.\n";
-                exit(EXIT_FAILURE);
-            }
-            tab.push_back(Arg(std::atoi(&s[pos])));
-        }
-        else
-        {
-            if (s[pos] == '*' || s[pos] == '/' || s[pos] == '+' || s[pos] == '-')
-            {
-				
-			}
-            else
-            {
-                std::cout << "Bad Input.\n";
-                exit(EXIT_FAILURE);
-            }
-        }
-        s = &s[pos + 1];
-        pos = s.find_first_not_of(' ');
-    }
->>>>>>> e29fa65f1e2eff51dc44fc566632c1eb6af85e86
 }
