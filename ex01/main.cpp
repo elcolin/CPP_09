efb61401e76b773a02e3d684fbfddd95ad1c79d6
Elise COLIN <ecolin@e1r1p6.clusters.42paris.fr>
2023-03-21 16:24:52 +0100

    elise est gentille et je l'aime

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elise <elise@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ecolin <ecolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:25:10 by elise             #+#    #+#             */
-/*   Updated: 2023/03/20 10:29:12 by elise            ###   ########.fr       */
+/*   Updated: 2023/03/20 17:04:29 by ecolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,50 @@ void print(std::list<int> &tab)
     std::cout << "\n";
 }
 
+void errorin(int con, const char *err_msg)
+{
+	if (con)
+	{
+		std::cout << err_msg << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	skip_space(char *str, int *i)
+{
+	while (str[*i] && str[*i] == ' ')
+		(*i)++;
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2 || !argv[1][0] || !isdigit(argv[1][0]))
-    {
-        std::cout << "Bad Input.\n";
-        exit(EXIT_FAILURE);
-    }
-    int i = 1;
+	errorin(argc != 2 || !argv[1][0], "Wrong Input.");
+    int i = 0;
+	int num = 0;
+	int op = 0;
     std::list<int> tab;
-    tab.push_back(argv[1][0]);
-    if (argv[1][i] != ' ')
-        exit(EXIT_FAILURE);
+	skip_space(argv[1], &i);
+	errorin(!isdigit(argv[1][i]), "Input has a non-digit character.");
+    tab.push_back(argv[1][i++]);
+	errorin(argv[1][i] != ' ', "Wrong Input");
     while (argv[1][i])
     {
-        if ((argv[1][i] == ' ' && argv[1][i - 1] == ' '))
-            exit(EXIT_FAILURE);
+		skip_space(argv[1], &i);
+		if (!argv[1][i])
+			break;
         if ((isdigit(argv[1][i]) || strchr("*/-+", argv[1][i])) && argv[1][i - 1] == ' ')
+		{
             tab.push_back(argv[1][i]);
-        else if (argv[1][i] != ' ')
-            exit(EXIT_FAILURE);
+			if (isdigit(argv[1][i]))
+				num++;
+			else
+				op++;
+		}
+        else if (argv[1][i] != ' ' && argv[1][i])
+			errorin(1, "Input has a non-digit character and is different then \" * / - + \"");
         i++;
     }
+	errorin(num != op, "Wrong quantity of numbers and signs.");
     std::list<int>::iterator it = tab.begin();
     std::list<int>::iterator tmp = tab.begin();
     std::list <int>::iterator r = tab.begin();
@@ -68,8 +90,7 @@ int main(int argc, char *argv[])
     {
         if (strchr("*/-+", *it) && it != r)
         {
-            it--;
-            it--;
+            errorin(it == tab.begin() || it-- == tab.begin() || it-- == tab.begin(), "Invalid Input.");
             *it = do_op(*it++ - '0', *it++ - '0', *it) + '0';
             r = it;
             it--;
